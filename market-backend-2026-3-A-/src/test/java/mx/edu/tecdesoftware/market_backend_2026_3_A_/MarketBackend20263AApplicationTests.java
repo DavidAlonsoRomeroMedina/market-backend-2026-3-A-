13992b97bb51000d8fd9dcdd98235b02db185637
DavidAlonsoRomeroMedina <david.romero@tecdesoftware.edu.mx>
2026-05-11 08:26:03 -0600

    chore: initial Spring Boot project setup

---
+++ market-backend-2026-3-A-/src/test/java/mx/edu/tecdesoftware/market_backend_2026_3_A_/MarketBackend20263AApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.tecdesoftware.market_backend_2026_3_A_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketBackend20263AApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
